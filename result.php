16d552e5292a895caf0406a2d0a5fc7abdd4af82
Christian-Lepidi <christianlepidi@gmail.com>
2024-02-23 16:44:34 +0100

    feat: error code

--- result.php
+++ result.php
@@ -1,3 +1,17 @@
 <?php
  $user_paragraph = $_GET["paragraph"];
- echo $user_paragraph;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1>
+    <?php echo $user_paragraph ?>
+   </h1> 
+</body>
+</html>
